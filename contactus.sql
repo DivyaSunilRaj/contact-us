4018eea499afef43ec3630f2728a1e1ac6cfae19
Divya Sunil Raj <A00316073@student.tus.ie>
2023-11-14 23:28:43 +0000

    database added

---
+++ contactus.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS contact;
+USE contact;
+CREATE TABLE contacts(
+UserName VARCHAR(50) NOT NULL,
+Email VARCHAR(50) NOT NULL,
+Message VARCHAR(500) );
+INSERT INTO contact(UserName,Email,Message)
+VALUES("abc","ab@c","awsome"); 
+INSERT INTO contact(UserName,Email,Message)
+VALUES("xyz","xy@c","delecious"); 
+INSERT INTO contact(UserName,Email,Message)
+VALUES("pqr","pq@c","superb"); 
+INSERT INTO contact(UserName,Email,Message)
+VALUES("lmn","lm@c","awsome"); 
+SELECT * FROM contact;
